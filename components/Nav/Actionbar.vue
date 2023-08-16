fbc2d20f5bb56fd13e0b410c80155688407c2809
Dominik <dominik.ziegenhagel@gmail.com>
2023-08-16 15:23:07 +0200

    add vueitfy

---
+++ components/Nav/Actionbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full p-4 flex">
+    <!-- have the title center and a settings icon on the right -->
+    <div class="flex-1 flex items-center">
+      <h1 class="text-2xl font-bold">{{ title }}</h1>
+    </div>
+    <div class="flex items-center gap-2">
+      <v-btn
+          @click="store.closeCurrentDevice"
+          variant="text"
+          color="blue"
+          size="large"
+          text="Fertig"/>
+    </div>
+  </div>
+</template>
