e01fc0d94b8fe478e93100dbb767e72081288ef0
Dominik <dominik.ziegenhagel@gmail.com>
2023-08-16 16:07:43 +0200

    add sample data

---
+++ composables/data.ts
@@ -0,0 +1,19 @@
+export const data = ref([
+    {path: 'photo01.jpg'},
+    {path: 'photo02.jpg'},
+    {path: 'photo03.jpg'},
+    {path: 'photo04.jpg'},
+    {path: 'photo05.jpg'},
+    {path: 'photo06.jpg'},
+    {path: 'photo07.jpg'},
+    {path: 'photo08.jpg'},
+    {path: 'photo09.jpg'},
+    {path: 'photo10.jpg'},
+    {path: 'photo11.jpg'},
+].map((item, index) => {
+    item.id = index + 1
+    item.path = '/data/' + item.path
+    item.grouped = index % 3 === 0
+    item.locked = index % 4 === 0
+    return item
+}))
--- pages/Verwaltungsmodus.vue
+++ pages/Verwaltungsmodus.vue
@@ -1,10 +1,26 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {data} from '~/composables/data.ts'
+</script>
 
 <template>
-  <div>
-    Layout: Verwaltungsmodus
-    <slot />
+  <!-- grid of 4 columns -->
+  <div class="grid grid-cols-5 gap-1 m-3 -mt-3">
+    <div v-for="element in data" :key="element.path" class="bg-white p-3 flex flex-col gap-3 rounded">
+
+      <div class="items-center justify-center flex">
+        &nbsp;
+        <v-icon icon="mdi-group" v-if="element.grouped" class="text-4xl"></v-icon>
+      </div>
+
+      <img :src="element.path" class="w-full object-cover aspect-square rounded" alt="">
+
+      <!-- left side have a lock open or closed, and right side an info icon -->
+      <div class="flex justify-between">
+        <v-icon icon="mdi-lock-open" v-if="element.locked" class="text-2xl"></v-icon>
+        <v-icon icon="mdi-lock" v-else class="text-2xl"></v-icon>
+        <v-icon icon="mdi-information-outline" class="text-2xl"></v-icon>
+      </div>
+
+    </div>
   </div>
 </template>
-
-<style scoped></style>
