f286d1fafd6a3d56f442dbe7b2ad565029448d32
Dominik <dominik.ziegenhagel@gmail.com>
2023-08-16 20:04:32 +0200

    planet view

---
+++ components/Explore/AllePlaneten.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {data} from '~/composables/data.ts'
+import {interaktionen} from '~/composables/interaktionen.ts'
+</script>
+<template>
+  <div class="flex gap-60 ml-80">
+    <Planet
+        v-bind="interaktionen[0]"
+        :images="data"
+    />
+    <Planet
+        v-bind="interaktionen[1]"
+        :images="data"
+    />
+  </div>
+  <div class="flex gap-60 -ml-80">
+    <Planet
+        v-bind="interaktionen[2]"
+        :images="data"
+    />
+    <Planet
+        v-bind="interaktionen[3]"
+        :images="data"
+    />
+  </div>
+</template>
--- pages/Explorationsmodus.vue
+++ pages/Explorationsmodus.vue
@@ -1,31 +1,10 @@
 <script lang="ts" setup>
-import {data} from '~/composables/data.ts'
-import {interaktionen} from '~/composables/interaktionen.ts'
 
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col items-center justify-center bg-gray-800">
-    <div class="flex gap-60 ml-80">
-      <Planet
-          v-bind="interaktionen[0]"
-          :images="data"
-      />
-      <Planet
-          v-bind="interaktionen[1]"
-          :images="data"
-      />
-    </div>
-    <div class="flex gap-60 -ml-80">
-      <Planet
-          v-bind="interaktionen[2]"
-          :images="data"
-      />
-      <Planet
-          v-bind="interaktionen[3]"
-          :images="data"
-      />
-    </div>
+    <ExploreAllePlaneten/>
   </div>
 </template>
 
