51e0ea6ce9486c565036d9df31dcdbc145cd3740
Dominik <dominik.ziegenhagel@gmail.com>
2023-08-16 16:31:59 +0200

    lupen ansicht planet

---
+++ components/Planet.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container text-white">
+    <div class="circle" :style="{backgroundColor: color}">
+      <v-icon :icon="icon" class="icon"/>
+      <h1>{{ title }}</h1>
+    </div>
+    <div class="image-container">
+      <div
+          v-for="(image, index) in images.slice(0, 6)"
+          :key="image"
+          class="image-circle bg-gray-400 border-4 border-white"
+          :style="positionImage(index, images.slice(0, 6).length)"
+      >
+        <img :src="image.path" alt="Image"
+             class="object-cover w-full h-full rounded-full"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  icon: String,
+  color: String,
+  images: Array,
+})
+
+const radius = 200;
+
+const positionImage = (index, totalImages) => {
+  const angle = (2 * Math.PI / totalImages) * index;
+  const x = 150 + radius * Math.cos(angle) - 25; // 25 ist die Hälfte der Breite und Höhe des Bildes
+  const y = 150 + radius * Math.sin(angle) - 25; // 25 ist die Hälfte der Breite und Höhe des Bildes
+  return `left: ${x}px; top: ${y}px;`;
+};
+</script>
+
+<style scoped>
+.container {
+  position: relative;
+  width: 400px;
+  height: 400px;
+}
+
+.circle {
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  position: absolute;
+  left: 50px;
+  top: 50px;
+
+  h1 {
+    font-size: 1.5rem;
+  }
+}
+
+.icon {
+  font-size: 4rem;
+}
+
+
+.image-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  margin-left: 50px;
+  margin-top: 50px;
+  align-items: center;
+  justify-content: center;
+}
+
+.image-circle {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  position: absolute;
+}
+</style>
--- pages/Explorationsmodus.vue
+++ pages/Explorationsmodus.vue
@@ -1,9 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {data} from '~/composables/data.ts'
+</script>
 
 <template>
-  <div>
-    Layout: Explorationsmodus
-    <slot />
+  <div class="inset-0 absolute flex flex-col items-center justify-center bg-gray-800">
+    <Planet
+        title="Vergrößern"
+        icon="mdi-magnify"
+        color="#97AD61"
+        :images="data"
+    />
   </div>
 </template>
 
