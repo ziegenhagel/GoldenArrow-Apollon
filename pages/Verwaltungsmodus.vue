14d8496384d2612b5a0060a81625dd3ff16ebbae
Dominik <dominik.ziegenhagel@gmail.com>
2023-08-16 20:13:50 +0200

    drag n drop

--- pages/Verwaltungsmodus.vue
+++ pages/Verwaltungsmodus.vue
@@ -1,12 +1,67 @@
 <script lang="ts" setup>
-import {data} from '~/composables/data.ts'
+import {data} from '~/composables/data.ts';
+
+const draggingElement = ref(null);
+
+const handleDragStart = (event, element) => {
+  draggingElement.value = element;
+  event.dataTransfer.effectAllowed = 'move';
+};
+
+const handleDrop = (event, targetElement) => {
+  // remove all the drag styling
+  event.target.parentElement.classList.remove('border-blue-400');
+  if (!draggingElement.value) return;
+
+  const draggedIndex = data.value.indexOf(draggingElement.value);
+  const targetIndex = data.value.indexOf(targetElement);
+
+  if (draggedIndex !== targetIndex) {
+    const [removed] = data.value.splice(draggedIndex, 1);
+    data.value.splice(targetIndex, 0, removed);
+  }
+
+  draggingElement.value = null;
+};
+
+const handleDragOver = event => {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+};
+
+const handleDragEnter = (event, element) => {
+  if (draggingElement.value === element) {
+    return;
+  }
+
+  const targetIndex = data.value.indexOf(element);
+  const draggedIndex = data.value.indexOf(draggingElement.value);
+
+  if (draggedIndex < targetIndex) {
+    event.target.parentElement.classList.add('border-blue-400');
+  } else {
+    event.target.parentElement.classList.add('border-blue-400');
+  }
+};
+
+const handleDragLeave = event => {
+  event.target.parentElement.classList.remove('border-blue-400');
+};
 </script>
 
 <template>
-  <!-- grid of 4 columns -->
   <div class="grid grid-cols-5 gap-1 m-3 mt-0">
-    <div v-for="element in data" :key="element.path" class="bg-white p-3 flex flex-col gap-3 rounded">
-
+    <div
+        v-for="element in data"
+        :key="element.path"
+        class="bg-white p-3 flex flex-col gap-3 rounded border-4 border-white"
+        draggable="true"
+        @dragstart="event => handleDragStart(event, element)"
+        @drop="event => handleDrop(event, element)"
+        @dragover="handleDragOver"
+        @dragenter="event => handleDragEnter(event, element)"
+        @dragleave="handleDragLeave"
+    >
       <div class="items-center justify-center flex">
         &nbsp;
         <v-icon icon="mdi-group" v-if="element.grouped" class="text-4xl"></v-icon>
@@ -24,3 +79,4 @@ import {data} from '~/composables/data.ts'
     </div>
   </div>
 </template>
+
