f51271117ca155c49d6a0ac7a8a64d83e31012fe
Dominik <dominik.ziegenhagel@gmail.com>
2023-08-16 16:37:29 +0200

    fix displays

--- pages/Explorationsmodus.vue
+++ pages/Explorationsmodus.vue
@@ -4,12 +4,34 @@ import {data} from '~/composables/data.ts'
 
 <template>
   <div class="inset-0 absolute flex flex-col items-center justify-center bg-gray-800">
-    <Planet
-        title="Vergrößern"
-        icon="mdi-magnify"
-        color="#97AD61"
-        :images="data"
-    />
+    <div class="flex gap-60 ml-80">
+      <Planet
+          title="Vergrößern"
+          icon="mdi-magnify"
+          color="#97AD61"
+          :images="data"
+      />
+      <Planet
+          title="Fokussieren"
+          icon="mdi-gesture-tap"
+          color="#B48DB0"
+          :images="data"
+      />
+    </div>
+    <div class="flex gap-60 -ml-80">
+      <Planet
+          title="Verknüpfung"
+          icon="mdi-trending-up"
+          color="#7D8DB6"
+          :images="data"
+      />
+      <Planet
+          title="Vergleichen"
+          icon="mdi-flip-horizontal"
+          color="#C18365"
+          :images="data"
+      />
+    </div>
   </div>
 </template>
 
